43b5bd1146d886047372b816f3c09c9ed98ff63e
Kiss Ferenc <ferenc.kiss@e-let.hu>
2014-05-10 20:10:45 +0200

    Adatbázis lekérdezést frissítettem, valamint a naptáraknál megjelenik szépen a naptár neve, edző neve, valamint átálltam pixeles top és height értékre százalékos helyett, mert nem volt pontos a százalékos.

--- bmefitness/functions/weekcalendar.php
+++ bmefitness/functions/weekcalendar.php
@@ -3,7 +3,7 @@
 	require_once("database.php");
 	$MINHOUR = 0;
 	$MAXHOUR = 23;
-	$TDWIDTH = 100;
+	$TDWIDTH = 120;
 	$TDHEIGHT = 30;
 
 	/*! Egy 7 tagú tömb lesz a végeredmény, így könnyen fogjuk tudni az adott nap adatait (például hétfő az a 0. elem)
@@ -114,14 +114,16 @@
 		$lastday .= " 23:59:59";
 
 		// forditva kell lekerdezni, tehat az ig, a befejezodesnek nagyobbnak kell lennie, mint az elso nap reggele, es a tol, azaz el kell kezdodnie a het utolso perce elott...
-		$where = "ig > cast('".$firstday."' AS timestamp) AND tol < cast('".$lastday."' AS timestamp)";
+		$where = "naptar.aktiv AND NOT naptar.torolve";
+		$where .= " AND naptar.ig > cast('".$firstday."' AS timestamp) AND naptar.tol < cast('".$lastday."' AS timestamp) AND naptar.ora = orak.id AND naptar.edzo = edzok.id";
 
-		$naptarak = db_select_all_data("fitness.naptar", $where);// "firstday: ".$firstday."<br>lastday: ".$lastday."<br>where: ".$where;
+		$naptarak = db_select_data("fitness.naptar, fitness.orak, fitness.edzok", "*, naptar.id AS naptar_id, orak.nev AS ora_nev, orak.id AS ora_id, edzok.id AS edzo_id, edzok.rovid_nev AS edzo_rovid_nev", $where, "");
 
 //		print $naptarak; return;
 
 		// szerintem az osszes datum szoveget atkonvertalom rendes datumra
 		for ($i = 0; $i < count($naptarak); $i++) {
+//			print "ora neve: ".$naptarak[$i]->ora_nev.", ora color = ".$naptarak[$i]->color."<br>";
 			$naptarak[$i]->tol = strtotime($naptarak[$i]->tol);
 			$naptarak[$i]->ig = strtotime($naptarak[$i]->ig);
 			$naptarak[$i]->torolve_mikor = strtotime($naptarak[$i]->torolve_mikor);
@@ -139,28 +141,6 @@
 		$maxhour = $GLOBALS['MAXHOUR'];
 		$tdwidth = $GLOBALS['TDWIDTH'];
 		$tdheight = $GLOBALS['TDHEIGHT'];
-
-/*
-		.bg {
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		width: 50%;
-		}
-		.content {
-		position: relative;
-			z-index: 1;
-		}
-		.yellow {
-		left: 0;
-			background-color: yellow;
-		}
-		.green {
-		right: 0;
-			background-color: green;
-		}
- */
-
 
 		print "<table class=\"calendartable\">\n";
 		for ($hours = $minhour - 1; $hours <= $maxhour; $hours++) {
@@ -183,18 +163,38 @@
 				}
 				// ekkor mar az orakat
 				else {
-
 					$tdcontent = "<div style=\"position: relative; width: 100%; height: ".$tdheight."px;\">";
 					foreach (orakAtHourOfDayInNaptarak($naptarak, date("Y", $weekdays[$weeks]), date("m", $weekdays[$weeks]), date("d", $weekdays[$weeks]), $hours) as $adat) {
-						$bcolor = "red";
+						// szin kiszedese
+						$bcolor = "#FFFFFF"; // feher lesz, ha nincs szine...
+						if ($adat->bejegyzes->color != "")
+							$bcolor = "#".$adat->bejegyzes->color;
+
+						// atalakitjuk, hogy osszefuggo legyen az egesz...
 						$amax = $adat->max == 59 ? 60 : $adat->max; // hogy teljesen ki legyen toltve...
-						$minm = $adat->min / 60 * 100;
-						$maxm = ($amax - $adat->min) / 60 * 100;
+						$minm = round($tdheight / 60 * $adat->min); // top...
+						$maxm = round($tdheight / 60 * ($amax - $adat->min)); // height
+
+						// ha min 0, akkor el kell felul tuntetni a bordert
 						if ($adat->min == 0)
 							$tdstyle .= " border-top-color: ".$bcolor.";";
+						// ha max 60, akkor meg alulrol tuntetjuk el a bordert
 						if ($amax == 60)
 							$tdstyle .= "border-bottom-color: ".$bcolor.";";
-						$tdcontent .= "<div style=\"position: absolute; width: 100%;".($adat->min == 0 ? "" : " top: ".$minm."%;").($amax >= 60 ? " height: 100%;" : " height: ".$maxm."%;")." background-color: ".$bcolor.";\"></div>";
+
+						// kiszamitjuj a magassagot, mert ez kell a naptar info kiiratasahoz is
+						$topsz = $adat->min == 0 ? "top: 0px;" : " top: ".$minm."px;";
+						$tdcontent .= "<div style=\"position: absolute; width: 100%;".$topsz.($amax >= 60 && $adat->min == 0 ? " height: 100%;" : " height: ".$maxm."px;")." background-color: ".$bcolor.";\"></div>";
+
+						// kiiratjuk a naptar nevet es egyeb infojat, ha ez a kezdodatum.
+						$mindate = date("Y", $weekdays[$weeks])."-".date("m", $weekdays[$weeks])."-".date("d", $weekdays[$weeks])." ".$hours.":".($adat->min < 10 ? "0".$adat->min : $adat->min);
+						if (date("Y-m-d H:i", $adat->bejegyzes->tol) == date("Y-m-d H:i", strtotime($mindate))) {
+							$tdcontent .= "<div style=\"position: relative; z-index: 1;".$topsz."\">".$adat->bejegyzes->ora_nev;
+							// ha nagyobb vagy egyenlo, mint 70 perc, akkor megjelenitjuk az edzo rovid nevet is, mert egyebkent nem fer ki...
+							if ((($adat->bejegyzes->ig - $adat->bejegyzes->tol) / 60) >= 70)
+								$tdcontent .= "<br>".$adat->bejegyzes->edzo_rovid_nev;
+							$tdcontent .= "</div>";
+						}
 					}
 					$tdcontent .= "</div>";
 				}
@@ -205,27 +205,6 @@
 			print "</tr>\n";
 		}
 		print "</table>\n";
-		/*
-		for ($i = 0; $i < count($weekdays); $i++) {
-			print "weekdate[".($i + 1)."]".date("Y", $weekdays[$i])." ".shortMonthName(date("n", $weekdays[$i]))." ".date("j", $weekdays[$i])."<br>";
-		}
-
-		$weekofyear = date("W", $thedate);
-		$year = date("Y", $thedate);
-		$month = date("m", $thedate);
-
-		$dayofmonth = date("j", $thedate);
-		$dayofweek = date("N", $thedate);
-		$numberofdaysinmonth = date("t", $thedate);
-
-		print "weekplusz: ".$weekplusz."<br>";
-		print "weekofyear: ".$weekofyear."<br>";
-		print "year: ".$year."<br>";
-		print "month: ".$month."<br>";
-		print "dayofmonth: ".$dayofmonth."<br>";
-		print "dayofweek: ".$dayofweek."<br>";
-		print "dd: ".($dayofmonth + $dayofweek)."<br>";
-		 */
 	}
 
 ?>
