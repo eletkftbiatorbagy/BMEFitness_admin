e695184551590dfa02dd062b7259c14d3907148e
Kiss Ferenc <ferenc.kiss@e-let.hu>
2014-06-26 00:19:08 +0200

    Elvileg előkészítettem a delegate funkciókat a foglalásokat kivéve.

--- bmefitness/functions/delegate.php
+++ bmefitness/functions/delegate.php
@@ -1,11 +1,159 @@
 <?php
 
-	/*	Hozzáadtunk egy új edzőt az adatbázishoz
+
+// EDZO
+	/*	Mielőtt hozzáadunk egy új edzőt az adatbázishoz
+	 *
+	 *	\param data			tartalma: id, foto, vnev, knev, rovid_nev, alcim, leiras, ertekeles
+	 */
+	function UjEdzoHozzaadasaElott($data) {
+//		print "uj edzo hozzaadasa elott: ".$data->vnev. " ".$data->knev."<br>";
+	}
+
+	/*	Miután hozzáadtunk egy új edzőt az adatbázishoz
 	 *
 	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
-	 *	\param edzodata		tartalma: id, foto, vnev, knev, rovid_nev, alcim, leiras, ertekeles
+	 *	\param data			tartalma: id, foto, vnev, knev, rovid_nev, alcim, leiras, ertekeles
 	 */
-	function UjEdzoHozzaadva($sikeres, $edzodata) {
-
+	function UjEdzoHozzaadasaUtan($sikeres, $data) {
+//		print "uj edzo hozzaadva: ".$data->vnev. " ".$data->knev."<br>";
 	}
+
+	/*	Mielőtt módosítunkg egy edzőt az adatbázisban
+	 *
+	 *	\param data			tartalma: id, foto, vnev, knev, rovid_nev, alcim, leiras, ertekeles
+	 */
+	function EdzoModositasaElott($data) {
+//		print "edzo modositasa elott: ".$data->vnev. " ".$data->knev."<br>";
+	}
+
+	/*	Miután módosítottunk egy edzőt az adatbázisban
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id, foto, vnev, knev, rovid_nev, alcim, leiras, ertekeles
+	 */
+	function EdzoModositasaUtan($sikeres, $data) {
+//		print "edzo modositva: ".$data->vnev. " ".$data->knev."<br>";
+	}
+
+
+
+
+// ORA
+	/*	Mielőtt hozzáadunk egy új órát az adatbázishoz
+	 *
+	 *	\param data			tartalma: id, foto, logo, nev, rovid_nev, alcim, leiras, max_letszam, perc, belepodij, color
+	 */
+	function UjOraHozzaadasaElott($data) {
+//		print "uj ora hozzaadasa elott: ".$data->nev."<br>";
+	}
+
+	/*	Miután hozzáadtunk egy új órát az adatbázishoz
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id, foto, logo, nev, rovid_nev, alcim, leiras, max_letszam, perc, belepodij, color
+	 */
+	function UjOraHozzaadasaUtan($sikeres, $data) {
+//		print "uj ora hozzaadva: ".$data->nev."<br>";
+	}
+
+	/*	Mielőtt módosítunkg egy órát az adatbázisban
+	 *
+	 *	\param data			tartalma: id, foto, logo, nev, rovid_nev, alcim, leiras, max_letszam, perc, belepodij, color
+	 */
+	function OraModositasaElott($data) {
+//		print "ora modositasa elott: ".$data->nev."<br>";
+	}
+
+	/*	Miután módosítottunk egy órát az adatbázisban
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id, foto, logo, nev, rovid_nev, alcim, leiras, max_letszam, perc, belepodij, color
+	 */
+	function OraModositasaUtan($sikeres, $data) {
+//		print "ora modositva: ".$data->nev."<br>";
+	}
+
+
+
+
+// TEREM
+	/*	Mielőtt hozzáadunk egy új termet az adatbázishoz
+	 *
+	 *	\param data			tartalma: id, foto, nev, alcim, foglalhato
+	 */
+	function UjTeremHozzaadasaElott($data) {
+//		print "uj terem hozzaadasa elott: ".$data->nev."<br>";
+	}
+
+	/*	Miután hozzáadtunk egy új termet az adatbázishoz
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id, foto, nev, alcim, foglalhato
+	 */
+	function UjTeremHozzaadasaUtan($sikeres, $data) {
+//		print "uj terem hozzaadva: ".$data->nev."<br>";
+	}
+
+	/*	Mielőtt módosítunkg egy termet az adatbázisban
+	 *
+	 *	\param data			tartalma: id, foto, nev, alcim, foglalhato
+	 */
+	function TeremModositasaElott($data) {
+//		print "terem modositasa elott: ".$data->nev."<br>";
+	}
+
+	/*	Miután módosítottunk egy termet az adatbázisban
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id, foto, nev, alcim, foglalhato
+	 */
+	function TeremModositasaUtan($sikeres, $data) {
+//		print "terem modositva: ".$data->nev."<br>";
+	}
+
+
+
+
+// Naptar
+// Mármint itt naptárbejegyzésekről van szó, tehát nem a foglalásokkal kapcsolatos delegate funkciók érkeznek be ide...
+// TODO: lehet, hogy nem csak az id-t kellene visszavárni a bme.js-ben
+	/*	Mielőtt hozzáadunk egy új naptárat az adatbázishoz
+	 *
+	 *	\param data			tartalma: id
+	 */
+	function UjNaptarHozzaadasaElott($data) {
+//		print "uj naptar hozzaadasa elott: ".$data->id."<br>";
+	}
+
+	/*	Miután hozzáadtunk egy új naptárat az adatbázishoz
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id
+	 */
+	function UjNaptarHozzaadasaUtan($sikeres, $data) {
+//		print "uj naptar hozzaadva: ".$data->id."<br>";
+	}
+
+	/*	Mielőtt módosítunkg egy naptárat az adatbázisban
+	 *
+	 *	\param data			tartalma: id
+	 */
+	function NaptarModositasaElott($data) {
+//		print "naptar modositasa elott: ".$data->id."<br>";
+	}
+
+	/*	Miután módosítottunk egy naptárat az adatbázisban
+	 *
+	 *	\param sikeres		igaz, ha a felvitel során nem jelenkezett hiba.
+	 *	\param data			tartalma: id
+	 */
+	function NaptarModositasaUtan($sikeres, $data) {
+//		print "naptar modositva: ".$data->id."<br>";
+	}
+
+
+
+// TODO: ide kellenek még a foglalásokkal kapcsolatos értesítések
+
 ?>
--- bmefitness/functions/insert_or_update_data.php
+++ bmefitness/functions/insert_or_update_data.php
@@ -1,10 +1,12 @@
 <?php
 	if (isset($_POST["data_id"]) && isset($_POST["table_name"]) && isset($_POST["schema"]) && isset($_POST["value_ids"]) && isset($_POST["values"]) && isset($_POST["returning"])) {
 		require_once("database.php");
+		require_once("delegate.php");
 		require_once("functions.php");
 
+		$shema = $_POST["schema"];
 		$data_id = $_POST["data_id"];
-		$tableNameWithSchema = $_POST["table_name"].".".$_POST["schema"];
+		$tableNameWithSchema = $_POST["table_name"].".".$shema;
 		$vauleIDs = $_POST["value_ids"];
 		$values = $_POST["values"];
 		$returning = $_POST["returning"];
@@ -13,6 +15,36 @@
 		if ($data_id != "-1")
 			$uj = false;
 
+
+		// felvitel elotti delegate...
+		if ($shema == "edzok") {
+			if ($uj)
+				UjEdzoHozzaadasaElott($objectum);
+			else
+				EdzoModositasaElott($objectum);
+		}
+		else if ($shema == "orak") {
+			if ($uj)
+				UjOraHozzaadasaElott($objectum);
+			else
+				OraModositasaElott($objectum);
+		}
+		else if ($shema == "termek") {
+			if ($uj)
+				UjTeremHozzaadasaElott($objectum);
+			else
+				TeremModositasaElott($objectum);
+		}
+		else if ($shema == "naptar") {
+			if ($uj)
+				UjNaptarHozzaadasaElott($objectum);
+			else
+				NaptarModositasaElott($objectum);
+		}
+
+
+
+		// adatbazis bejegyzes...
 		if ($data_id == "-1")
 			$result_object = db_insert_into_table($tableNameWithSchema, $vauleIDs, $values, $returning);
 		else if ($data_id == "all") // $data_id lehet 'all' is!
@@ -21,15 +53,50 @@
 			$result_object = db_update_rows_in_table($data_id, $tableNameWithSchema, $vauleIDs, $values, $returning);
 
 
+		// kiertekeles...
+		$sikeres = false;
+		$objectum = NULL;
 		// megint at kell alakitani, hogy jo legyen a new_or_edit_data_forms.php-ban..
 		if (is_array($result_object) && count($result_object) > 0) {
 			$ojson = json_from_object($result_object[0]);
+			$sikeres = true;
+			$objectum = $result_object[0];
 		}
 		else if ($result_object == NULL) {
-			$ojson = "error";
+			$ojson = "error"; // eleve false a sikeres
 		}
 		else {
 			$ojson = json_from_object($result_object);
+			$sikeres = true; // bar ez inkabb hibas, mint jo lenne...
+			$objectum = $result_object;
+		}
+
+
+
+		// visszajelzesek felvitel utan...
+		if ($shema == "edzok") {
+			if ($uj)
+				UjEdzoHozzaadasaUtan($sikeres, $objectum);
+			else
+				EdzoModositasaUtan($sikeres, $objectum);
+		}
+		else if ($shema == "orak") {
+			if ($uj)
+				UjOraHozzaadasaUtan($sikeres, $objectum);
+			else
+				OraModositasaUtan($sikeres, $objectum);
+		}
+		else if ($shema == "termek") {
+			if ($uj)
+				UjTeremHozzaadasaUtan($sikeres, $objectum);
+			else
+				TeremModositasaUtan($sikeres, $objectum);
+		}
+		else if ($shema == "naptar") {
+			if ($uj)
+				UjNaptarHozzaadasaUtan($sikeres, $objectum);
+			else
+				NaptarModositasaUtan($sikeres, $objectum);
 		}
 
 		print $ojson;
