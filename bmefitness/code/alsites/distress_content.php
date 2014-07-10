c464e2985fac2ad0c7c37ee2b86bd99136a875e3
Kiss Ferenc <ferenc.kiss@e-let.hu>
2014-07-10 17:58:36 +0200

    Foglalásoknál is működnek a “ma” és a hétváltó gombok.

--- bmefitness/code/alsites/distress_content.php
+++ bmefitness/code/alsites/distress_content.php
@@ -9,8 +9,8 @@
 	if (isset($_POST['torolt']) && $_POST['torolt'] === "true")
 		$only_torolt = true;
 
-	// TODO: itt meg nem jo az onclick het valtoztatasa!
-	print "<br><span onclick=\"change_timetable_het(".($het - 1).");\" class=\"action_button\"><</span><span style=\"font-size: 2em;\"> ".(date("W") + $het).". hét </span><span onclick=\"change_timetable_het(".($het + 1).");\" class=\"action_button\">></span><br><br>";
+	print "<div onclick=\"change_het(0, 'distress');\" class=\"action_button\" style=\"width: 80px; float: left;\">ma</div><br>";
+	print "<span onclick=\"change_het(".($het - 1).", 'distress');\" class=\"action_button\"><</span><span style=\"font-size: 2em;\"> ".(date("W") + $het).". hét </span><span onclick=\"change_het(".($het + 1).", 'distress');\" class=\"action_button\">></span><br><br>";
 	printFoglalasokTable(false, $only_torolt, $het);
 	// TODO: csak egy termet kell megjeleniteni egyszerre
 ?>
--- bmefitness/code/alsites/distress_settings.php
+++ bmefitness/code/alsites/distress_settings.php
@@ -1,4 +1,4 @@
 <?php
-	print "<div onclick=\"change_timetable_het(0);\" class=\"action_button\">most</div>";
+//	print "<div onclick=\"change_het(0, 'distress');\" class=\"action_button\">ma</div>";
 	print "<div id=\"change_distress_torolt_button\" onclick=\"change_distress_torolt(true);\" class=\"action_button\">Törölt foglalások</div>";
 ?>
