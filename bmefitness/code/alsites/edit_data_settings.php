accda4e08a36383f9dcc1112e94017c75cca4e53
Kiss Ferenc <ferenc.kiss@e-let.hu>
2014-07-10 21:17:04 +0200

    Az adatok gombjait megváltoztattam. Resolved #22

--- bmefitness/code/alsites/edit_data_settings.php
+++ bmefitness/code/alsites/edit_data_settings.php
@@ -1,8 +1,6 @@
-<ul>
-	<!-- <li><a onclick="change_edit_data_site('info', null)">Infó</a></li> -->
-	<li><a onclick="change_edit_data_site('edzok', null)">Edzők</a></li>
-	<!-- <li><a onclick="change_edit_data_site('felhasznalok', null)">Felhasználók</a></li> -->
-	<li><a onclick="change_edit_data_site('termek', null)">Termek</a></li>
-	<li><a onclick="change_edit_data_site('orak', null)">Órák</a></li>
-	<!-- <li><a onclick="change_edit_data_site('beallitasok', null)">Beállítások</a></li> -->
-</ul>
+<!-- <div onclick="change_edit_data_site('info', null)" class="action_button">Infó</div> -->
+<div onclick="change_edit_data_site('edzok', null)" class="action_button">Edzők</div>
+<!-- <div onclick="change_edit_data_site('felhasznalok', null)" class="action_button">Felhasználók</div> -->
+<div onclick="change_edit_data_site('termek', null)" class="action_button">Termek</div>
+<div onclick="change_edit_data_site('orak', null)" class="action_button">Órák</div>
+<!-- <div onclick="change_edit_data_site('beallitasok', null)" class="action_button">Beállítások</div> -->
