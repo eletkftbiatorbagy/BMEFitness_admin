1217fa195709abc3ec3da3ae5c06e0c53d1e3ef3
Kiss Ferenc <ferenc.kiss@e-let.hu>
2014-07-10 21:05:46 +0200

    Kész van a foglalások táblázat jelmagyarázata! Resolved #21

--- bmefitness/code/alsites/distress_content.php
+++ bmefitness/code/alsites/distress_content.php
@@ -16,4 +16,11 @@
 	print "<div onclick=\"change_het(0, 'distress');\" class=\"action_button\" style=\"width: 80px; float: left;\">ma</div><br>";
 	print "<span onclick=\"change_het(".($het - 1).", 'distress');\" class=\"action_button\"><</span><span style=\"font-size: 2em;\"> ".(date("W") + $het).". hét </span><span onclick=\"change_het(".($het + 1).", 'distress');\" class=\"action_button\">></span><br><br>";
 	printFoglalasokTable(false, $only_torolt, $terem, $het);
+
+	print "<br><table style=\"margin-left: auto; margin-right: auto;\">";
+	print "<tr><td style=\"text-align: center;\"><div style=\"padding: 5px; margin: 5px; width: 400px; background-color: #99FF75;\">El van fogadva a foglalás, és nem ütközik másik eseménnyel.</div></td>";
+	print "<td style=\"text-align: center;\"><div style=\"padding: 5px; margin: 5px; width: 400px; background-color: yellow;\">El van fogadva a foglalás, de ütközik egy másik eseménnyel.</div></td></tr>";
+	print "<tr><td style=\"text-align: center;\"><div style=\"padding: 5px; margin: 5px; width: 400px; background-color: red; color: white;\">Nincs elfogadva a foglalás, és ütközik egy másik eseménnyel.</div>";
+	print "<td style=\"text-align: center;\"><div style=\"padding: 5px; margin: 5px; width: 400px; background-color: white;\">Nincs elfogadva a foglalás, és nem ütközik másik eseménnyel.</div></tr>";
+	print "</table>";
 ?>
